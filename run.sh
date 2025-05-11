49e408b03ab9be7a303f579169c278f3cb35e9fe
BorisGrechkin <yoshimitsu878@gmail.com>
2025-05-11 12:42:26 +0400

    fix

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker run --gpus all --rm \
   -v $(pwd)/Data:/app/Data \
   -v $(pwd)/Visualization:/app/Visualization \
   -v $(pwd)/Model_weights:/app/Model_weights \
-  boris_ai_model
+  seq_to_seq_model
